b92ffce644a79b4a7797e03d73bc4fa77798757d
LucasAlvesDaCosta <40143263+LucasAlvesDaCosta@users.noreply.github.com>
2018-07-03 16:11:41 -0300

    nova versão do crud e das funcionalidades
    alterados hoje

--- pesquisar_cliente.php
+++ pesquisar_cliente.php
@@ -5,14 +5,14 @@
      </head>
      <body>
       <?=$ident = $_POST['select'];?>
-      <?php 
-         if($ident == Titulo)
-           $ident = 'Autor';
+      <?php
+        if($ident == Nome)
+        $ident = 'UF';
       ?>
      <table class="table table-striped table-bordered table-condensed table-hover" width="450px" border="3" cellspacing="1">
 	  <tr>
         <td><strong>numero</strong></td>
-	    <td><strong>Titulo</strong></td>
+	    <td><strong>nome</strong></td>
 	    <td><strong><?=$ident?></strong></td>
       </tr> 
     
@@ -26,59 +26,44 @@
     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
     
     $pesquisar = $_POST['pesquisar'];
-    $result_cursos = "SELECT titulo,autor FROM livros WHERE titulo LIKE '%$pesquisar%' ";
+    $result_cursos = "SELECT Nome,uf FROM cliente WHERE Nome LIKE '%$pesquisar%' ";
     $resultado_cursos = mysqli_query($conn, $result_cursos);
     $i =1;
     $t = $_POST['select'];
           
     echo ' = Resultado para: ' .$t.'<br>';
-    if($t == Titulo){
+    if($t == Nome){
        while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
            echo '<tr>';
         echo '<td>' .$i.'</td>';
-        echo '<td>' .$rows_cursos['titulo'].'</td>';
-        echo '<td>' .$rows_cursos['autor'].'</td>';
+        echo '<td>' .$rows_cursos['Nome'].'</td>';
+        echo '<td>' .$rows_cursos['uf'].'</td>';
         echo '</tr>';
        $i +=1;
       } 
     }
-         if( $t == Autor){
-            $result_cursos = "SELECT titulo, autor FROM livros WHERE autor LIKE '%$pesquisar%' ";
+         if( $t == Email){
+            $result_cursos = "SELECT Nome,Email FROM cliente WHERE Email LIKE '%$pesquisar%' ";
             $resultado_cursos = mysqli_query($conn, $result_cursos);
             
             while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
                 echo '<tr>';
                 echo '<td>' .$i.'</td>';
-                echo ' <td>' .$rows_cursos['titulo']. ' </td>';
-                echo '<td>' .$rows_cursos['autor'].'</td>';
+                echo ' <td>' .$rows_cursos['Nome']. ' </td>';
+                echo '<td>' .$rows_cursos['Email'].'</td>';
                 echo '</tr>';
                $i +=1;
           }
         }
-              if($t == Categoria){
-                $ident= 'Categoria';
-                $result_cursos = "SELECT titulo,categoria FROM livros WHERE categoria LIKE '%$pesquisar%' ";
+              if($t == Cidade){
+                $result_cursos = "SELECT Nome,cidade FROM cliente WHERE cidade LIKE '%$pesquisar%' ";
                 $resultado_cursos = mysqli_query($conn, $result_cursos);
 
                 while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
                     echo '<tr>';
                     echo '<td>' .$i.'</td>';
-                    echo '<td>' .$rows_cursos['titulo'].'</td>';
-                    echo '<td>' .$rows_cursos['categoria'].'</td>';
-                    echo '</tr>';
-                   $i +=1;
-              }
-            }
-
-            if($t == ISBN){
-                $result_cursos = "SELECT titulo,ISBN FROM livros WHERE ISBN LIKE '%$pesquisar%' ";
-                $resultado_cursos = mysqli_query($conn, $result_cursos);
-
-                while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
-                    echo '<tr>';
-                    echo '<td>' .$i.'</td>';
-                    echo '<td>' .$rows_cursos['titulo'].'</td>';
-                    echo '<td>' .$rows_cursos['ISBN'].'</td>';
+                    echo '<td>' .$rows_cursos['Nome'].'</td>';
+                    echo '<td>' .$rows_cursos['cidade'].'</td>';
                     echo '</tr>';
                    $i +=1;
               }
